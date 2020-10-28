5e52ae1e8efdfd1141ce3edbf9dd2a795ac19603
Leo <leonardo.antazu@upeu.edu.pe>
2020-10-28 12:08:54 -0500

    conexionoracle

---
+++ Sysrubricas-2020/src/main/java/pe/edu/com/lp2/Sysrubricas2020Application.java
@@ -0,0 +1,13 @@
+package pe.edu.com.lp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sysrubricas2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sysrubricas2020Application.class, args);
+	}
+
+}
