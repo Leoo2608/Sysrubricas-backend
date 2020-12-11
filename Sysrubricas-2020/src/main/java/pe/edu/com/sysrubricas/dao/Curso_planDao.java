3bf19dbafc32e8e6b7b6628ac3d588c3e122630e
Jorge <jorgequispes@upeu.edu.pe>
2020-12-11 13:18:16 -0500

    SUPER COMMIT 2.0 , CASI EL ULTIMO COMMIT

---
+++ Sysrubricas-2020/src/main/java/pe/edu/com/sysrubricas/dao/Curso_planDao.java
@@ -0,0 +1,15 @@
+package pe.edu.com.sysrubricas.dao;
+
+import java.util.Map;
+
+import pe.edu.com.sysrubricas.entity.Curso_plan;
+
+public interface Curso_planDao {
+int create (Curso_plan cp);
+int update (Curso_plan cp);
+int delete (int id);
+Map<String, Object> readAll();
+Map<String, Object> read(int id);
+Map<String, Object> readcpp(int id);
+
+}
--- Sysrubricas-2020/src/test/java/pe/edu/com/sysrubricas/Sysrubricas2020ApplicationTests.java
+++ Sysrubricas-2020/src/test/java/pe/edu/com/sysrubricas/Sysrubricas2020ApplicationTests.java
@@ -4,13 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import pe.edu.com.sysrubricas.dao.CursoDao;
+import pe.edu.com.sysrubricas.dao.Curso_planDao;
 import pe.edu.com.sysrubricas.dao.LineaAcademicaDao;
-
+import pe.edu.com.sysrubricas.dao.ParticipanteDao;
 import pe.edu.com.sysrubricas.dao.PlanAcademicoDao;
 import pe.edu.com.sysrubricas.dao.PlanLineaDao;
 import pe.edu.com.sysrubricas.dao.UnidadAcademicaDao;
+import pe.edu.com.sysrubricas.entity.Curso;
+import pe.edu.com.sysrubricas.entity.Curso_plan;
 import pe.edu.com.sysrubricas.entity.LineaAcademica;
-
+import pe.edu.com.sysrubricas.entity.Participante;
 import pe.edu.com.sysrubricas.entity.PlanAcademico;
 import pe.edu.com.sysrubricas.entity.PlanLinea;
 
@@ -21,21 +25,18 @@ class Sysrubricas2020ApplicationTests {
 	private PlanAcademicoDao pd;
 	@Autowired
 	private UnidadAcademicaDao ud;
+	@Autowired
+	private ParticipanteDao p;
+	@Autowired
+	private CursoDao cu;
+	@Autowired
+	private Curso_planDao cp;
+	
 	@Test
 	void contextLoads() {
-
-		/*PlanAcademico p = new PlanAcademico();
-		p.setIdplan(23);
-		p.setNombre("2016-1 EP Ingeniería de Sistemas - Sede Lima");
-		p.setIdunidad(5);
-		p.setCiclos(10);
-		p.setCreditos(201);
-		p.setCursos(70);
-		p.setAnio_inicio("02/03/2016");
-		p.setAnio_termino("25/12/2020");
-		System.out.println(pd.update(p));*/
-		//System.out.println(ud.Campus());
-		System.out.println(pd.readxUnidad(5));
+						
+						
+		System.out.println(cp.read(85));
 	}	
 
 }
